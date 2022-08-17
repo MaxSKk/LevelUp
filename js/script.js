77c05e88bfb3382af481c44b69216ce5314dec0d
MaxSk <maxskokov4@gmail.com>
2022-08-17 18:32:29 +0300

    add order block

--- js/script.js
+++ js/script.js
@@ -5,4 +5,14 @@ $(function () {
       fade: true,
       cssEase: 'linear'
    });
+
+   $('a[href*="#"]').on('click', function () {
+      $('html, body').animate({
+         scrollTop: $($.attr(this, 'href')).offset().top
+      }, 400);
+      return false;
+   });
+
+   blocktimer('#timer', '2010, 7, 1', 30 * 60 * 1000);
+
 });
